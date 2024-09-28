8b4e365a7c323317bb25ec069f2c00a14d6b3120
akirakabuki <167601701+akirakabuki@users.noreply.github.com>
2024-09-28 18:25:47 +0900

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pulp
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from packaging.version import Version
+import japanize_matplotlib 
 # ShiftSchedulerクラスとoptimization関数をインポート
 from ShiftScheduler import ShiftScheduler
 
